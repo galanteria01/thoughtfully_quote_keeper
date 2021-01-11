09018f9a3edc34cd4b69657dbe891b7873a9a555
galanteria01 <shanuu12e@gmail.com>
2021-01-11 16:02:33 +0530

    Adding addquote function

---
+++ lib/AddQuote.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddNote extends StatefulWidget {
+  @override
+  _AddNoteState createState() => _AddNoteState();
+}
+
+class _AddNoteState extends State<AddNote> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
